173ed60bd1240cc6753301233c99a5d73780acf1
hwlv <3025947168@qq.com>
2022-06-13 19:13:13 +0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Button from './components/Button';
-import { BrowserRouter, Routes, Route,Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LifeCycle from './pages/LifeCycle';
 
 import './style.css';
@@ -16,7 +16,6 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
@@ -27,12 +26,9 @@ export default function App() {
         <Link to="/expenses">Expenses</Link>
       </div>
       <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="lifeCycle" element={<LifeCycle />} />
-    </Routes>
-
+        <Route path="/" element={<App />} />
+        <Route path="lifeCycle" element={<LifeCycle />} />
+      </Routes>
     </div>
-  </BrowserRouter>
-
   );
 }
--- index.tsx
+++ index.tsx
@@ -8,7 +8,8 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  
+    <BrowserRouter>
     <App />
+  </BrowserRouter>
    
 );
